7d323921ffcd247ba30d8fe853096cef9214f6b5
TarraKan <61558634+BoyNextDoor-228@users.noreply.github.com>
2025-12-09 11:03:12 +0300

    fix(app): Fixed wrong OnboardingScreen documentation

--- app/lib/feature/onboarding/screen/onboarding_screen.dart
+++ app/lib/feature/onboarding/screen/onboarding_screen.dart
@@ -13,8 +13,6 @@ class OnboardingScreen extends StatefulWidget {
     super.key,
   });
 
-  /// Creates a widget, which is used to let IRL user set his initial weight.
-  ///
   /// [onOnboardingPassed] is a callback, which is called, when user passes
   /// onboarding.
   final VoidCallback onOnboardingPassed;
